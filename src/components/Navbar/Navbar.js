431d71b44c8d5ef276eeacbb65df1bd074f22b07
Kanchan Jadon <kanchanjadon96@gmail.com>
2024-01-23 22:14:34 +0530

    navbar update

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,14 +3,14 @@ import Logo from './Logo'
 import styles from './Navbar.module.css'
 import SearchBar from './SearchBar'
 import Button from './Button'
-import {useNavigate} from 'react-router-dom'
+
 
 
 const Navbar = () => {
-    const navigate= useNavigate();
+    
   return (
     <div className={styles.navbar}>
-        <div className={styles.logo} onClick={()=>navigate('/')}><Logo /></div>
+        <div className={styles.logo} ><Logo /></div>
         <SearchBar/>
         <Button btnText="Give Feedback"/>
     </div>
--- src/components/Navbar/SearchBar.js
+++ src/components/Navbar/SearchBar.js
@@ -1,18 +1,21 @@
-import React from 'react'
-import styles from './Navbar.module.css'
-import  {ReactComponent as SearchIcon }   from '../../assets/searchIcon.svg'
+import React from "react";
+import styles from "./Navbar.module.css";
+import { ReactComponent as SearchIcon } from "../../assets/searchIcon.svg";
 
 const SearchBar = () => {
   return (
     <>
-    
-        <form className={styles.searchBarContainer}>
-        <input placeholder='Search a song of your choice' className={styles.searchBarInput}></input>
-        <div type='submit' className={styles.searchBarButton}><SearchIcon/></div>
-        </form>
-    
+      <form className={styles.searchBarContainer}>
+        <input
+          placeholder="Search a song of your choice"
+          className={styles.searchBarInput}
+        ></input>
+        <button type="submit" className={styles.searchBarButton}>
+          <SearchIcon />
+        </button>
+      </form>
     </>
-  )
-}
+  );
+};
 
-export default SearchBar
+export default SearchBar;
