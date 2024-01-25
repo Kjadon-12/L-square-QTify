fa27df6355fb6cc6a538fe419b4411ee280fbb77
Kanchan Jadon <kanchanjadon96@gmail.com>
2024-01-25 14:39:54 +0530

    herosection

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -1,24 +1,22 @@
-import React from 'react'
-import styles from './HeroSection.module.css'
-import HeroImage from '../../assets/heroImage.svg'
+import React from "react";
+import styles from "./HeroSection.module.css";
+import HeroImage from "../../assets/heroImage.svg";
 
 const HeroSection = () => {
   return (
-    <>
-    <section className={styles.wrapper}>
-      {/* <div className={styles.heroSectionContainer}> */}
+    <div className={styles.hero}>
+      
       <div className={styles.heroSectionText}>
-						<h1>100 Thousand Songs, ad free</h1>
-						<h1>Over thousands podcast episodes</h1>
-					</div>
-                    
-                        <img src={HeroImage} alt='hero-section'></img>
-                    
-        
-       {/* </div>   */}
-    </section>
-    </>
-  )
-}
+        <h1>100 Thousand Songs, ad free</h1>
+        <h1>Over thousands podcast episodes</h1>
+      </div>
+      <div>
+        <img src={HeroImage} alt="hero-section" width={212}></img>
+      </div>
 
-export default HeroSection
+      
+    </div>
+  );
+};
+
+export default HeroSection;
